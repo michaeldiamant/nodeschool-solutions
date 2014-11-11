2a8f2fa0622bb0191e79fcfbce36532edcfefae3
mdiamant <michael.diamant@mediacrossing.com>
2014-11-11 16:17:57 -0500

    Solve exercises 10-13.

---
+++ learnyounode/13.js
@@ -0,0 +1,60 @@
+var http = require('http');
+var url = require('url');
+
+function go(parsedUrl, response) {
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+  var requestTimestamp = new Date(parsedUrl.query.iso);
+  response.end(
+  JSON.stringify({
+    'hour' : requestTimestamp.getHours(),
+    'minute' : requestTimestamp.getMinutes(),
+    'second' : requestTimestamp.getSeconds(),
+  }));
+}
+
+function hms(timestamp) {
+  return {
+    'hour' : timestamp.getHours(),
+    'minute' : timestamp.getMinutes(),
+    'second' : timestamp.getSeconds(),
+  };
+}
+
+function epoch(timestamp) {
+  return { 'unixtime' : timestamp.getTime() };
+}
+
+function completeResponse(parsedUrl, response, formatTimestamp) {
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+  response.end(
+    JSON.stringify(
+      formatTimestamp(
+        new Date(parsedUrl.query.iso))));
+}
+
+function schonfinkelize(fn) {
+  var slice = Array.prototype.slice,
+    stored_args = slice.call(arguments, 1);
+  return function () {
+    var new_args = slice.call(arguments),
+      args = stored_args.concat(new_args);
+        return fn.apply(null, args);
+  };
+}
+
+http.createServer(function (request, response) {
+  var parsedUrl = url.parse(request.url, true);
+  var complete = schonfinkelize(completeResponse, parsedUrl, response);
+  switch (parsedUrl.pathname) {
+    case '/api/parsetime':
+      complete(hms);
+      break;
+    case '/api/unixtime':
+      complete(epoch);
+      break;
+    default:
+      response.writeHead(400, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify({ 'error' : 'Unsupported route ' + parsedUrl.pathname }));
+  }  
+})
+  .listen(process.argv[2]);
